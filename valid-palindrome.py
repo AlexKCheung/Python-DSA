bb3c21607723813d989c4fc9b4fae0b29ddaf6ae
Alex Cheung <alexcheung880@gmail.com>
2024-02-16 14:43:44 -0800

    faster and better memory

--- valid-palindrome.py
+++ valid-palindrome.py
@@ -1,15 +1,25 @@
 def isPalindrome(self, s: str) -> bool:
     # helper functions .isalnum() .lower()
-    # stack: until halfway, then pop stack until end 
-    # iterate until half while checking if s[i] == -s[i]
-    # can one pass this way
+    # another method stack: until halfway, then pop stack until end
 
-    new_string = []
-    for i in range(len(s)): 
-        if s[i].isalnum(): 
-            new_string.append(s[i].lower())
-    for i in range(len(new_string) // 2): 
-        if new_string[i] != new_string[-(i + 1)]: 
+    # new_string = []
+    # for i in range(len(s)): 
+    #     if s[i].isalnum(): 
+    #         new_string.append(s[i].lower())
+    # for i in range(len(new_string) // 2): 
+    #     if new_string[i] != new_string[-(i + 1)]: 
+    #         return False
+    # return True
+
+    # faster two pointer 
+    l, r = 0, len(s) - 1
+    while l < r: 
+        while l < r and not s[l].isalnum(): 
+            l += 1
+        while l < r and not s[r].isalnum(): 
+            r -= 1
+        if s[l].lower() != s[r].lower(): 
             return False
+        l += 1
+        r -= 1
     return True
-    
