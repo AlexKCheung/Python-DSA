ffadc9651bd3bf312719360290e83057c31a395d
Alex Cheung <alexcheung880@gmail.com>
2024-02-16 16:27:54 -0800

    binary search

---
+++ binary-search.py
@@ -0,0 +1,13 @@
+def search(self, nums: List[int], target: int) -> int:
+    left, right = 0, len(nums) - 1
+
+    while left <= right: 
+        # middle = left + (right - left) // 2
+        middle = (left + right) // 2
+        if nums[middle] == target: 
+            return middle
+        if nums[middle] > target: 
+            right = middle - 1
+        elif nums[middle] < target: 
+            left = middle + 1
+    return -1
