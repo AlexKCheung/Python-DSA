af155ad196611b967a2ae3e3029ef055158100d0
Alex Cheung <alexcheung880@gmail.com>
2024-08-29 19:17:33 -0700

    it appears i have done this problem before

--- invert-binary-tree.py
+++ invert-binary-tree.py
@@ -4,7 +4,21 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
-    if root: 
-        root.left, root.right = self.invertTree(root.right), self.invertTree(root.left)
+# def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+#     if root: 
+#         root.left, root.right = self.invertTree(root.right), self.invertTree(root.left)
+#     return root
+
+def invert(self, root):
+    if not root:
+        return
+    root.left, root.right = root.right, root.left
+    self.invert(root.left)
+    self.invert(root.right)
+    
     return root
+
+def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+    if not root:
+        return root
+    return self.invert(root)
