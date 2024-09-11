08f6cafc4fabd9e508388f0980d15e4735d08528
Alex Cheung <alexcheung880@gmail.com>
2024-09-11 00:27:15 -0700

    frustrated with myself, so back to the basics we need to learn

--- valid-parentheses.py
+++ valid-parentheses.py
@@ -1,15 +1,32 @@
+# def isValid(self, s: str) -> bool:
+#     parentheses = {'(': ')', '{': '}', '[': ']'}
+#     sentence = []
+#     for i in range(len(s)): 
+#         if s[i] in parentheses: 
+#             sentence.append(s[i])
+#         # we're given only ()[]{} in s
+#         else: 
+#             if len(sentence) == 0: 
+#                 return False
+#             elif parentheses[sentence.pop()] == s[i]:
+#                 continue
+#             else: 
+#                 return False
+#     return len(sentence) == 0
+
+
 def isValid(self, s: str) -> bool:
-    parentheses = {'(': ')', '{': '}', '[': ']'}
-    sentence = []
-    for i in range(len(s)): 
-        if s[i] in parentheses: 
-            sentence.append(s[i])
-        # we're given only ()[]{} in s
-        else: 
-            if len(sentence) == 0: 
+    queue = []
+    brackets = {"(": ")", "[": "]", "{": "}"}
+
+    for i in s:
+        if i in brackets.keys():
+            queue.append(i)
+        else:
+            if len(queue) > 0:
+                top = queue.pop()
+                if brackets[top] != i:
+                    return False
+            else:
                 return False
-            elif parentheses[sentence.pop()] == s[i]:
-                continue
-            else: 
-                return False
-    return len(sentence) == 0
+    return len(queue) == 0
