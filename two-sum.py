7e12da7dfdb0384e39786b8f4196413493cb7ffe
Alex Cheung <alexcheung880@gmail.com>
2024-10-15 19:20:41 -0700

    doing neetcode 150 to refresh

--- two-sum.py
+++ two-sum.py
@@ -1,10 +1,18 @@
 def twoSum(self, nums: List[int], target: int) -> List[int]:
-    # have dictionary where key, value = nums value, index
-    values = {}
-    for i in range(len(nums)): 
-        if target - nums[i] in values: 
-            return [i, values[target - nums[i]]]
-        else: 
-            values[nums[i]] = i
+    pairs = {}
+    for i in range(len(nums)):
+        if target - nums[i] in pairs:
+            return [i, pairs[target - nums[i]]]
+        else:
+            pairs[nums[i]] = i
+        
+# def twoSum(self, nums: List[int], target: int) -> List[int]:
+#     # have dictionary where key, value = nums value, index
+#     values = {}
+#     for i in range(len(nums)): 
+#         if target - nums[i] in values: 
+#             return [i, values[target - nums[i]]]
+#         else: 
+#             values[nums[i]] = i
     
-    return [0,1]
+#     return [0,1]
