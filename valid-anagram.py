bebbfb8d7f0438a244ed4309417029e1517dfffb
Alex Cheung <alexcheung880@gmail.com>
2024-10-15 19:15:29 -0700

    aha i know python ord now

--- valid-anagram.py
+++ valid-anagram.py
@@ -1,20 +1,32 @@
 def isAnagram(self, s: str, t: str) -> bool:
-    counter = {}
-    for i in range(len(s)): 
-        if s[i] in counter: 
-            counter[s[i]] += 1
-        else: 
-            counter[s[i]] = 1
-
-    for i in range(len(t)): 
-        if not t[i] in counter: 
-            return False
-        else: 
-            counter[t[i]] -= 1
-    
-    for i in counter: 
-        if counter[i] != 0: 
+    alphabet = [0] * 26
+    for i in s:
+        alphabet[ord(i) - ord('a')] += 1
+    for i in t:
+        alphabet[ord(i) - ord('a')] -= 1
+        
+    for i in alphabet:
+        if i != 0:
             return False
     return True
 
-    # can also do return sorted(s) == sorted(t)
+# def isAnagram(self, s: str, t: str) -> bool:
+#     counter = {}
+#     for i in range(len(s)): 
+#         if s[i] in counter: 
+#             counter[s[i]] += 1
+#         else: 
+#             counter[s[i]] = 1
+
+#     for i in range(len(t)): 
+#         if not t[i] in counter: 
+#             return False
+#         else: 
+#             counter[t[i]] -= 1
+    
+#     for i in counter: 
+#         if counter[i] != 0: 
+#             return False
+#     return True
+
+#     # can also do return sorted(s) == sorted(t)
