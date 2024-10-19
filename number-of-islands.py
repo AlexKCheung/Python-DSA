92ccf3391c88640cfda66ca8bf6fc4f4a5f11134
Alex Cheung <alexcheung880@gmail.com>
2024-10-19 16:37:06 -0700

    .

--- number-of-islands.py
+++ number-of-islands.py
@@ -1,29 +1,19 @@
 def numIslands(self, grid: List[List[str]]) -> int:
-    # bfs each island to mark as seen 
-    islands = 0
-    # add edges of the grid with a 0 water to handle out of bounds in bfs
-    for row in grid:
-        row.insert(0, "0")
-        row.append("0")
+    island_count = 0
+    def dfs(r, c):
+        if r < 0 or c < 0 or r == len(grid) or c == len(grid[0]) or grid[r][c] != '1':
+            return
 
-    grid.insert(0, ["0"] * len(grid[0]))
-    grid.append(["0"] * len(grid[0]))
+        grid[r][c] = '-1'
+        dfs(r + 1, c)
+        dfs(r - 1, c)
+        dfs(r, c + 1)
+        dfs(r, c - 1)
 
-    def dfs(grid, row_num, col_num):
-        if grid[row_num][col_num] == "1":
-            grid[row_num][col_num] = "-1"
-            dfs(grid, row_num + 1, col_num)
-            dfs(grid, row_num - 1, col_num)
-            dfs(grid, row_num, col_num + 1)
-            dfs(grid, row_num, col_num - 1)
+    for row in range(len(grid)):
+        for col in range(len(grid[0])):
+            if grid[row][col] == '1':
+                island_count += 1
+                dfs(row, col)
 
-
-    # ignore first and last as we know they're water and out of bounds
-    for row in range(1, len(grid) - 1):
-        for col in range(1, len(grid[0]) - 1):
-            if grid[row][col] == "1":
-                islands += 1
-                dfs(grid, row, col)
-    
-    return islands
-    
+    return island_count
