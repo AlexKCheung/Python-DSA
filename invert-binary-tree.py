b4eb803054049a9044899cfa68ec11c82d18ad45
Alex Cheung <alexcheung880@gmail.com>
2024-10-18 23:53:41 -0700

    another update

--- invert-binary-tree.py
+++ invert-binary-tree.py
@@ -4,21 +4,9 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-# def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
-#     if root: 
-#         root.left, root.right = self.invertTree(root.right), self.invertTree(root.left)
-#     return root
-
-def invert(self, root):
-    if not root:
-        return
-    root.left, root.right = root.right, root.left
-    self.invert(root.left)
-    self.invert(root.right)
-    
-    return root
 
 def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
     if not root:
         return root
-    return self.invert(root)
+    root.left, root.right = self.invertTree(root.right), self.invertTree(root.left) 
+    return root
