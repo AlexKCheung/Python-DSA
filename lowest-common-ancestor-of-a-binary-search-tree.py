a59e6db650dc2047bf4aaa358d880f1940ec8810
Alex Cheung <alexcheung880@gmail.com>
2024-10-20 16:31:38 -0700

    update

--- lowest-common-ancestor-of-a-binary-search-tree.py
+++ lowest-common-ancestor-of-a-binary-search-tree.py
@@ -6,11 +6,11 @@
 #         self.right = None
 
 def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-    curr = root
-    while curr:
-        if curr.val < p.val and curr.val < q.val:
-            curr = curr.right
-        elif curr.val > p.val and curr.val > q.val:
-            curr = curr.left
+    while root:
+        if p.val > root.val and q.val > root.val:
+            root = root.right
+        elif p.val < root.val and q.val < root.val:
+            root = root.left
         else:
-            return curr
+            return root
+    
