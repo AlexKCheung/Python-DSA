bcb6ffc0c4c054d8596566afc6ab8fff501e6d3d
Alex Cheung <alexcheung880@gmail.com>
2024-10-22 17:37:15 -0700

    oops

--- best-time-to-buy-and-sell-stock.py
+++ best-time-to-buy-and-sell-stock.py
@@ -1,10 +1,7 @@
 def maxProfit(self, prices: List[int]) -> int:
-    min_day = prices[0]
-    max_profit = 0
-
-    for i in range(len(prices)): 
-        if prices[i] < min_day:
-            min_day = prices[i]
-        max_profit = max(max_profit, prices[i] - min_day)
-
-    return max_profit
+    profit = 0
+    buy_price = prices[0]
+    for i in prices:
+        profit = max(profit, i - buy_price)
+        buy_price = min(buy_price, i)
+    return profit
