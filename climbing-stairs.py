77219ade8c054501ec2ba8ff01d45b16bbbfb0f3
Alex Cheung <alexcheung880@gmail.com>
2024-10-28 23:46:35 -0700

    time for dp practice

--- climbing-stairs.py
+++ climbing-stairs.py
@@ -1,10 +1,12 @@
 def climbStairs(self, n: int) -> int:
-    cur = 1
-    prev = 1
-
-    for i in range(n - 1): 
-        temp = cur
-        cur = cur + prev
-        prev = temp
-
-    return cur
+    if n == 1:
+        return 1
+    prev_two = 0
+    prev_one = 1
+    output = 0
+    for i in range(n):
+        output = prev_one + prev_two
+        prev_two = prev_one
+        prev_one = output
+    return output
+    
