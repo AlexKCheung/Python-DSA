5303d4d3b6a9452b6599677f2eabdf538508cf3f
Alex Cheung <alexcheung880@gmail.com>
2024-10-29 23:06:30 -0700

    unique paths

---
+++ unique-paths.py
@@ -0,0 +1,9 @@
+def uniquePaths(self, m: int, n: int) -> int:
+    row = [1] * n 
+    # m - 1 because bottom row is all 1's
+    for i in range(m - 1):
+        new_row = [1] * n
+        for j in range(n - 2, -1, -1):
+            new_row[j] = new_row[j + 1] + row[j]
+        row = new_row
+    return row[0]
