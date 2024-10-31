f21fbbec115d7c3d9c94b042312c55c43be110a3
Alex Cheung <alexcheung880@gmail.com>
2024-10-30 19:47:43 -0700

    done again

--- merge-intervals.py
+++ merge-intervals.py
@@ -1,12 +1,10 @@
 def merge(self, intervals: List[List[int]]) -> List[List[int]]:
-    intervals.sort(key = lambda x: x[0])
-    output = []
-    output.append(intervals[0])
-    for i in range(1, len(intervals)):
-        if intervals[i][0] <= output[-1][1]:
-            bigger_end = max(output[-1][1], intervals[i][1])
-            output[-1] = [output[-1][0], bigger_end]
-        else:
-            output.append(intervals[i])
-    return output
+    intervals.sort(key = lambda x:x[0])
+    output = [intervals[0]]
 
+    for i in intervals:
+        if i[0] <= output[-1][1]:
+            output[-1][1] = max(output[-1][1], i[1])
+        else:
+            output.append(i)
+    return output
