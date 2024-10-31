2e1a10a8ae049306caf77222428e8f2242766989
Alex Cheung <alexcheung880@gmail.com>
2024-10-31 13:40:54 -0700

    oh alr done before

--- merge-two-sorted-lists.py
+++ merge-two-sorted-lists.py
@@ -5,19 +5,18 @@
 #         self.next = next
 
 def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-    temp = merged_list = ListNode()
-    while list1 and list2: 
-        if list1.val < list2.val: 
-            merged_list.next = list1
-            list1 = list1.next
-        else: 
-            merged_list.next = list2
+    head = output = ListNode()
+    while list1 and list2:
+        if list1.val > list2.val:
+            output.next = list2
             list2 = list2.next
-        merged_list = merged_list.next
-        
-    if list1: 
-        merged_list.next = list1
-    if list2: 
-        merged_list.next = list2
-    
-    return temp.next
+        else:
+            output.next = list1
+            list1 = list1.next
+        output = output.next
+    if list1:
+        output.next = list1
+    if list2:
+        output.next = list2
+
+    return head.next
